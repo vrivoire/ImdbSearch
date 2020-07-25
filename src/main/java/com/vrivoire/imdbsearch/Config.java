4b4622322df777548c0c7e09a0733c9a9aee7881
Vincent Rivoire <rivoire.vincent@bell.ca>
2020-07-25 18:48:06 -0400

    new RollingFile

--- src/main/java/com/vrivoire/imdbsearch/Config.java
+++ src/main/java/com/vrivoire/imdbsearch/Config.java
@@ -3,6 +3,7 @@ package com.vrivoire.imdbsearch;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -17,6 +18,7 @@ import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,129 +27,125 @@ import org.apache.logging.log4j.Logger;
  * @author Vincent
  */
 public enum Config {
-    WEB_BROWSER,
-    ICON,
-    REPORT_NAME,
-    REPORT_HEADER,
-    REPORT_BODY,
-    REPORT_FOOTER,
-    PATTERN,
-    SUPPORTED_EXTENSIONS,
-    TORRENTS;
+	WEB_BROWSER,
+	ICON,
+	REPORT_NAME,
+	REPORT_HEADER,
+	REPORT_BODY,
+	REPORT_FOOTER,
+	PATTERN,
+	SUPPORTED_EXTENSIONS,
+	TORRENTS,
+	IGNORED_FOLDERS;
 
-    private static final Logger LOG = LogManager.getLogger(Config.class);
-    private static final Map<String, Object> MAP = new HashMap<>();
-    private static Thread thread = null;
+	private static final Logger LOG = LogManager.getLogger(Config.class);
+	private static final Map<String, Object> MAP = new HashMap<>();
+	private static Thread thread = null;
 
-    static {
-        try {
-            final ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+	static {
+		try {
+			final ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
 
-            InputStream is = new NameYearBean().getClass().getResourceAsStream("/config.json");
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader reader = new BufferedReader(isr);
-            StringBuilder sb = new StringBuilder();
-            reader.lines().forEachOrdered((String line) -> {
-                sb.append(line);
-            }
-            );
-            String defaults = sb.toString();
-            MAP.putAll(objectMapper.readValue(defaults, new TypeReference<Map<String, Object>>() {
-            }));
-            LOG.info("Default configuration " + (defaults != null && !defaults.isEmpty() ? "" : "not") + " found");
-            LOG.debug("Default configuration:\n" + defaults);
+			InputStream is = new NameYearBean().getClass().getResourceAsStream("/config.json");
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader reader = new BufferedReader(isr);
+			StringBuilder sb = new StringBuilder();
+			reader.lines().forEachOrdered((String line) -> {
+				sb.append(line);
+			}
+			);
+			String defaults = sb.toString();
+			MAP.putAll(objectMapper.readValue(defaults, new TypeReference<Map<String, Object>>() {
+			}));
+			LOG.info("Default configuration " + (defaults != null && !defaults.isEmpty() ? "" : "not") + " found");
+			LOG.debug("Default configuration:\n" + defaults);
 
-            File file = new File("config.json");
-            if (!file.exists()) {
-                file = new File("bin/config.json");
-            }
-            if (!file.exists() && defaults == null) {
-                LOG.fatal("Config file not found. exiting.");
-                if (thread != null) {
-                    try {
-                        thread.interrupt();
-                    }
-                    catch (SecurityException e) {
-                        LOG.error(e.getMessage(), e);
-                    }
-                }
-                System.exit(-1);
-            }
-            if (file.exists()) {
-                LOG.info("Config file found in: " + file.getAbsolutePath());
+			File file = new File("config.json");
+			if (!file.exists()) {
+				file = new File("bin/config.json");
+			}
+			if (!file.exists() && defaults == null) {
+				LOG.fatal("Config file not found. exiting.");
+				if (thread != null) {
+					try {
+						thread.interrupt();
+					} catch (SecurityException e) {
+						LOG.error(e.getMessage(), e);
+					}
+				}
+				System.exit(-1);
+			}
+			if (file.exists()) {
+				LOG.info("Config file found in: " + file.getAbsolutePath());
 
-                MAP.putAll(objectMapper.readValue(file, new TypeReference<Map<String, Object>>() {
-                }));
-                LOG.debug("Configuration:\n" + objectMapper.writeValueAsString(MAP));
+				MAP.putAll(objectMapper.readValue(file, new TypeReference<Map<String, Object>>() {
+				}));
+				LOG.debug("Configuration:\n" + objectMapper.writeValueAsString(MAP));
 
-                StartFileWatchDog(file);
+				StartFileWatchDog(file);
 
-            } else {
-                LOG.info("Using default configuration.");
-                MAP.putAll(objectMapper.readValue(defaults, new TypeReference<Map<String, Object>>() {
-                }));
-            }
-        }
-        catch (IOException ex) {
-            LOG.fatal(ex.getMessage(), ex);
-            if (thread != null) {
-                try {
-                    thread.interrupt();
-                }
-                catch (SecurityException e) {
-                    LOG.error(e.getMessage(), e);
-                }
-            }
-            System.exit(-1);
-        }
-    }
+			} else {
+				LOG.info("Using default configuration.");
+				MAP.putAll(objectMapper.readValue(defaults, new TypeReference<Map<String, Object>>() {
+				}));
+			}
+		} catch (IOException ex) {
+			LOG.fatal(ex.getMessage(), ex);
+			if (thread != null) {
+				try {
+					thread.interrupt();
+				} catch (SecurityException e) {
+					LOG.error(e.getMessage(), e);
+				}
+			}
+			System.exit(-1);
+		}
+	}
 
-    private static void StartFileWatchDog(File file) {
-        thread = new Thread("fileWatchDog") {
-            @Override
-            public void run() {
-                try {
-                    LOG.info("Thread " + getName() + " started.");
-                    WatchService watchService = FileSystems.getDefault().newWatchService();
-                    Path path = Paths.get(file.getParentFile().getAbsolutePath());
-                    path.register(
-                            watchService,
-                            StandardWatchEventKinds.ENTRY_CREATE,
-                            StandardWatchEventKinds.ENTRY_DELETE,
-                            StandardWatchEventKinds.ENTRY_MODIFY);
-                    WatchKey key;
+	private static void StartFileWatchDog(File file) {
+		thread = new Thread("fileWatchDog") {
+			@Override
+			public void run() {
+				try {
+					LOG.info("Thread " + getName() + " started.");
+					WatchService watchService = FileSystems.getDefault().newWatchService();
+					Path path = Paths.get(file.getParentFile().getAbsolutePath());
+					path.register(
+							watchService,
+							StandardWatchEventKinds.ENTRY_CREATE,
+							StandardWatchEventKinds.ENTRY_DELETE,
+							StandardWatchEventKinds.ENTRY_MODIFY);
+					WatchKey key;
 
-                    while ((key = watchService.take()) != null) {
-                        for (WatchEvent<?> event : key.pollEvents()) {
-                            if (event.kind().name().equals("ENTRY_MODIFY")) {
-                                LOG.info(getName() + ", event kind:" + event.kind() + ". File affected: " + event.context() + ".");
-                            }
-                        }
-                        key.reset();
-                    }
-                }
-                catch (IOException | InterruptedException ex) {
-                    LOG.fatal(ex.getMessage(), ex);
-                    if (thread != null) {
-                        try {
-                            thread.interrupt();
-                        }
-                        catch (SecurityException e) {
-                            LOG.error(e.getMessage(), e);
-                        }
-                    }
-                    System.exit(-1);
-                }
-            }
-        };
-        thread.start();
-    }
+					while ((key = watchService.take()) != null) {
+						for (WatchEvent<?> event : key.pollEvents()) {
+							if (event.kind().name().equals("ENTRY_MODIFY")) {
+								LOG.info(getName() + ", event kind:" + event.kind() + ". File affected: " + event.context() + ".");
+							}
+						}
+						key.reset();
+					}
+				} catch (IOException | InterruptedException ex) {
+					LOG.fatal(ex.getMessage(), ex);
+					if (thread != null) {
+						try {
+							thread.interrupt();
+						} catch (SecurityException e) {
+							LOG.error(e.getMessage(), e);
+						}
+					}
+					System.exit(-1);
+				}
+			}
+		};
+		thread.start();
+	}
 
-    public String getString() {
-        return (String) MAP.get(name());
-    }
+	public String getString() {
+		return (String) MAP.get(name());
+	}
 
-    public Object get() {
-        return MAP.get(name());
-    }
+	public Object get() {
+		return MAP.get(name());
+	}
 }
--- src/main/java/com/vrivoire/imdbsearch/SearchMovie.java
+++ src/main/java/com/vrivoire/imdbsearch/SearchMovie.java
@@ -5,6 +5,7 @@ import com.omertron.omdbapi.OmdbApi;
 import com.omertron.omdbapi.model.OmdbVideoFull;
 import com.omertron.omdbapi.tools.OmdbBuilder;
 import com.omertron.omdbapi.tools.OmdbParameters;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -20,6 +21,7 @@ import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.collections.IteratorUtils;
 import org.apache.logging.log4j.LogManager;
@@ -31,160 +33,161 @@ import org.apache.logging.log4j.Logger;
  */
 public class SearchMovie {
 
-    private static final Logger LOG = LogManager.getLogger(SearchMovie.class);
+	private static final Logger LOG = LogManager.getLogger(SearchMovie.class);
 
-    // BanMePlz
-    // 6cc19241
-    private final static OmdbApi OMDB_API = new OmdbApi("6cc19241");
-    private final List<NameYearBean> NOT_FOUND;
-    private final Pattern PATTERN = Pattern.compile(Config.PATTERN.getString());
+	// BanMePlz
+	// 6cc19241
+	private final static OmdbApi OMDB_API = new OmdbApi("6cc19241");
+	private final List<NameYearBean> NOT_FOUND;
+	private final Pattern PATTERN = Pattern.compile(Config.PATTERN.getString());
 
-    /**
-     *
-     */
-    public SearchMovie() {
-        NOT_FOUND = new ArrayList<>();
-    }
+	/**
+	 *
+	 */
+	public SearchMovie() {
+		NOT_FOUND = new ArrayList<>();
+	}
 
-    /**
-     *
-     * @return @throws FileNotFoundException
-     */
-    public List<NameYearBean> search() throws FileNotFoundException {
-        Set<NameYearBean> movieSet = listFiles();
-        final List<NameYearBean> list = new ArrayList<>();
-        movieSet.forEach((var nameYearBean) -> {
-            try {
-                NameYearBean search = searchiMDB(nameYearBean);
-                list.add(search);
-            }
-            catch (OMDBException | IllegalAccessException | InvocationTargetException ex) {
-                NOT_FOUND.add(nameYearBean);
-                LOG.info(nameYearBean);
-                LOG.info(ex.getMessage());
-                LOG.info(' ');
-            }
-        });
-        return list;
-    }
+	/**
+	 *
+	 * @return @throws FileNotFoundException
+	 */
+	public List<NameYearBean> search() throws FileNotFoundException {
+		Set<NameYearBean> movieSet = listFiles();
+		final List<NameYearBean> list = new ArrayList<>();
+		movieSet.forEach((var nameYearBean) -> {
+			try {
+				NameYearBean search = searchiMDB(nameYearBean);
+				list.add(search);
+			} catch (OMDBException | IllegalAccessException | InvocationTargetException ex) {
+				NOT_FOUND.add(nameYearBean);
+				LOG.info(nameYearBean);
+				LOG.info(ex.getMessage());
+				LOG.info(' ');
+			}
+		});
+		return list;
+	}
 
-    /**
-     *
-     * @return
-     */
-    public List<NameYearBean> getNoFound() {
-        return NOT_FOUND;
-    }
+	/**
+	 *
+	 * @return
+	 */
+	public List<NameYearBean> getNoFound() {
+		return NOT_FOUND;
+	}
 
-    private NameYearBean searchiMDB(NameYearBean nameYearBean) throws OMDBException, IllegalAccessException, InvocationTargetException {
-        OmdbParameters build;
-        Integer year = null;
-        try {
-            year = Integer.parseInt(nameYearBean.getYear());
-        }
-        catch (Exception nfe) {
-            // ignore, no year
-        }
-        if (year != null && year > 1800) {
-            build = new OmdbBuilder().setTitle(nameYearBean.getName()).setYear(year).build();
-        } else {
-            build = new OmdbBuilder().setTitle(nameYearBean.getName()).build();
-        }
+	private NameYearBean searchiMDB(NameYearBean nameYearBean) throws OMDBException, IllegalAccessException, InvocationTargetException {
+		OmdbParameters build;
+		Integer year = null;
+		try {
+			year = Integer.parseInt(nameYearBean.getYear());
+		} catch (Exception nfe) {
+			// ignore, no year
+		}
+		if (year != null && year > 1800) {
+			build = new OmdbBuilder().setTitle(nameYearBean.getName()).setYear(year).build();
+		} else {
+			build = new OmdbBuilder().setTitle(nameYearBean.getName()).build();
+		}
 
-        OmdbVideoFull info;
-        try {
-            info = OMDB_API.getInfo(build);
-        }
-        catch (OMDBException ex) {
-            build = new OmdbBuilder().setTitle(nameYearBean.getName()).build();
-            info = OMDB_API.getInfo(build);
-        }
-        BeanUtils.copyProperties(nameYearBean, info);
-        return nameYearBean;
-    }
+		OmdbVideoFull info;
+		try {
+			info = OMDB_API.getInfo(build);
+		} catch (OMDBException ex) {
+			build = new OmdbBuilder().setTitle(nameYearBean.getName()).build();
+			info = OMDB_API.getInfo(build);
+		}
+		BeanUtils.copyProperties(nameYearBean, info);
+		return nameYearBean;
+	}
 
-    private Set<NameYearBean> listFiles() throws FileNotFoundException {
-        var path = Path.of(Main.default_path);
-        Set<NameYearBean> nameYearBeanSet = new HashSet<>();
-        LOG.info(Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS) + " " + path);
-        if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
-            try {
-                @SuppressWarnings("unchecked")
-                List<String> extensions = (List<String>) Config.SUPPORTED_EXTENSIONS.get();
-                StringBuilder sb = new StringBuilder("glob:**.{");
-                extensions.forEach((extension) -> {
-                    sb.append(extension.strip().replace(".", "")).append(',');
-                });
-                sb.deleteCharAt(sb.lastIndexOf(","));
-                sb.append("}");
+	private Set<NameYearBean> listFiles() throws FileNotFoundException {
+		var path = Path.of(Main.default_path);
+		Set<NameYearBean> nameYearBeanSet = new HashSet<>();
+		LOG.info(Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS) + " " + path);
+		if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
+			try {
+				@SuppressWarnings("unchecked")
+				List<String> ignoredFolders = (List<String>) Config.IGNORED_FOLDERS.get();
+				@SuppressWarnings("unchecked")
+				List<String> extensions = (List<String>) Config.SUPPORTED_EXTENSIONS.get();
+				StringBuilder sb = new StringBuilder("glob:**.{");
+				extensions.forEach((extension) -> {
+					sb.append(extension.strip().replace(".", "")).append(',');
+				});
+				sb.deleteCharAt(sb.lastIndexOf(","));
+				sb.append("}");
 
-                var includeFilter = path.getFileSystem().getPathMatcher(sb.toString());
+				var includeFilter = path.getFileSystem().getPathMatcher(sb.toString());
 
-                Files.list(path).filter(p -> Files.isDirectory(p, LinkOption.NOFOLLOW_LINKS) || includeFilter.matches(p))
-                        .forEach((Path p) -> {
-                            String fileName = p.getFileName().toString();
-                            List<String> tokenized = tokenize(fileName);
-                            String fullFileName = p.getParent().toString() + File.separator + fileName;
-                            NameYearBean nameYearBean = getFileNameYear(tokenized, p.getFileName().toString(), new File(fullFileName).lastModified());
-                            nameYearBeanSet.add(nameYearBean);
-                        });
+				Files.list(path).filter(p -> Files.isDirectory(p, LinkOption.NOFOLLOW_LINKS) || includeFilter.matches(p))
+						.forEach((Path p) -> {
+							String fileName = p.getFileName().toString();
+							List<String> tokenized = tokenize(fileName);
+							String fullFileName = p.getParent().toString() + File.separator + fileName;
+							if (new File(fullFileName).isDirectory() && ignoredFolders.contains(fileName)) {
+								LOG.info("Ignoring folder: " + fileName);
+							} else {
+								NameYearBean nameYearBean = getFileNameYear(tokenized, p.getFileName().toString(), new File(fullFileName).lastModified());
+								nameYearBeanSet.add(nameYearBean);
+							}
+						});
 
-            }
-            catch (IOException ex) {
-                LOG.error(ex.getMessage(), ex);
-            }
-        }
-        return nameYearBeanSet;
-    }
+			} catch (IOException ex) {
+				LOG.error(ex.getMessage(), ex);
+			}
+		}
+		return nameYearBeanSet;
+	}
 
-    @SuppressWarnings("unchecked")
-    private List<String> tokenize(String fileName) {
-        for (String torrent : (List<String>) Config.TORRENTS.get()) {
-            final String fn = fileName.toLowerCase();
-            final String t = torrent.toLowerCase();
-            if (fn.contains(t)) {
-                int index = fn.indexOf(t);
-                fileName = fn.substring(0, index) + fn.substring(index + t.length());
-            }
-        }
+	@SuppressWarnings("unchecked")
+	private List<String> tokenize(String fileName) {
+		for (String torrent : (List<String>) Config.TORRENTS.get()) {
+			final String fn = fileName.toLowerCase();
+			final String t = torrent.toLowerCase();
+			if (fn.contains(t)) {
+				int index = fn.indexOf(t);
+				fileName = fn.substring(0, index) + fn.substring(index + t.length());
+			}
+		}
 
-        fileName = fileName.replace('_', ' ')
-                .replace('.', ' ').replace('-', ' ').replace('(', ' ').replace(')', ' ')
-                .replace('[', ' ').replace(']', ' ').replace('_', ' ')
-                .trim()
-                .replaceAll(" +", " ");
-        StringTokenizer tokenizer = new StringTokenizer(fileName, " ");
-        return (List<String>) IteratorUtils.toList(tokenizer.asIterator());
-    }
+		fileName = fileName.replace('_', ' ')
+				.replace('.', ' ').replace('-', ' ').replace('(', ' ').replace(')', ' ')
+				.replace('[', ' ').replace(']', ' ').replace('_', ' ')
+				.trim()
+				.replaceAll(" +", " ");
+		StringTokenizer tokenizer = new StringTokenizer(fileName, " ");
+		return (List<String>) IteratorUtils.toList(tokenizer.asIterator());
+	}
 
-    private NameYearBean getFileNameYear(List<String> tokenized, String originalName, long lastModified) {
-        Iterator iterator = tokenized.iterator();
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(iterator.next()).append(' ');
-        Integer year = null;
-        while (iterator.hasNext()) {
-            String next = ((String) iterator.next()).trim();
-            try {
-                year = Integer.parseInt(next);
-                if (year > 1800) {
-                    break;
-                }
-            }
-            catch (Exception nfe) {
-            }
+	private NameYearBean getFileNameYear(List<String> tokenized, String originalName, long lastModified) {
+		Iterator iterator = tokenized.iterator();
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(iterator.next()).append(' ');
+		Integer year = null;
+		while (iterator.hasNext()) {
+			String next = ((String) iterator.next()).trim();
+			try {
+				year = Integer.parseInt(next);
+				if (year > 1800) {
+					break;
+				}
+			} catch (Exception nfe) {
+			}
 
-            Matcher matcher = PATTERN.matcher(next);
-            if (matcher.find()) {
-                break;
-            }
-            stringBuilder.append(next).append(' ');
-        }
+			Matcher matcher = PATTERN.matcher(next);
+			if (matcher.find()) {
+				break;
+			}
+			stringBuilder.append(next).append(' ');
+		}
 
-        NameYearBean nameYearBean = new NameYearBean();
-        nameYearBean.setName(stringBuilder.toString().trim().toLowerCase());
-        nameYearBean.setYear(year == null ? null : year.toString());
-        nameYearBean.setFileDate(lastModified);
-        nameYearBean.setOriginalName(originalName);
-        return nameYearBean;
-    }
+		NameYearBean nameYearBean = new NameYearBean();
+		nameYearBean.setName(stringBuilder.toString().trim().toLowerCase());
+		nameYearBean.setYear(year == null ? null : year.toString());
+		nameYearBean.setFileDate(lastModified);
+		nameYearBean.setOriginalName(originalName);
+		return nameYearBean;
+	}
 }
