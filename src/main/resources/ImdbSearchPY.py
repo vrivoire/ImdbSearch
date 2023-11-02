c971a380fb4ebcd20b489003aa3ab7a335921980
vrivoire <rivoire.vincent@gmail.com>
2023-11-02 05:34:30 -0400

    Cleanup

--- src/main/resources/ImdbSearchPY.py
+++ src/main/resources/ImdbSearchPY.py
@@ -138,9 +138,8 @@ def path_search(path):
     args_search(files)
 
 
-if __name__ == "__main__":
-    start = datetime.timestamp(datetime.now())
-
+def pre_test():
+    global line
     master_version = ''
     for line in urllib.request.urlopen('https://raw.githubusercontent.com/cinemagoer/cinemagoer/master/imdb/version.py'):
         master_version = line.decode('utf-8')
@@ -156,14 +155,43 @@ if __name__ == "__main__":
         print("********************************************************************************************************************************")
         print("********************************************************************************************************************************")
 
-    print('********************* Test to see if "awards" are still broken *********************')
     cinemagoer = Cinemagoer()
-
     test = cinemagoer.get_movie(34583, info=['main', 'awards'])
+    print('********************* Test to see if "awards" are still broken *********************')
     print(f"movie['title']: {test['title']}, movie.get('awards'): {test.get('awards')}, ia.get_movie_awards(movie.movieID): {cinemagoer.get_movie_awards(test.movieID)}")
-    print('************************************************************************************')
+    if "movie['title']: Casablanca, movie.get('awards'): None, ia.get_movie_awards(movie.movieID): {'data': {}, 'titlesRefs': {}, 'namesRefs': {}}" != f"movie['title']: {test['title']}, movie.get('awards'): {test.get('awards')}, ia.get_movie_awards(movie.movieID): {cinemagoer.get_movie_awards(test.movieID)}":
+        print("********************************************************************************************************************************")
+        print("********************************************************************************************************************************")
+        print("********************************************************************************************************************************")
+        print(f'                                 "awards" are changed')
+        print("********************************************************************************************************************************")
+        print("********************************************************************************************************************************")
+        print("********************************************************************************************************************************")
 
-    CONFIG = json.load(open("{HOMEDRIVE}{HOMEPATH}/Documents/NetBeansProjects/ImdbSearch/bin/config.json".format(**os.environ)))
+
+def get_config_path():
+    global file_path
+    file_path = 'config.json'
+    if os.path.exists(file_path):
+        pass
+    else:
+        file_path = 'bin/' + file_path
+        if os.path.exists(file_path):
+            pass
+        else:
+            file_path = "{HOMEDRIVE}{HOMEPATH}/Documents/NetBeansProjects/ImdbSearch/bin/config.json"
+    print(f'Config path: {file_path}')
+    return file_path
+
+
+if __name__ == "__main__":
+    start = datetime.timestamp(datetime.now())
+
+    pre_test()
+
+    file_path = get_config_path()
+    CONFIG = json.load(open(file_path.format(**os.environ)))
+
     for line in CONFIG:
         CONFIG[line] = str(CONFIG[line]).replace('${', '{').format(**os.environ)
     SUPPORTED_EXTENSIONS = tuple(CONFIG["SUPPORTED_EXTENSIONS"])
