74a3fa057e2a230c06a23e650037227fd597238a
vrivoire <rivoire.vincent@gmail.com>
2024-08-28 08:20:24 -0400

    batch

---
+++ src/main/resources/Videos.bat
@@ -0,0 +1,6 @@
+@echo off
+
+start %HOMEDRIVE%%HOMEPATH%\Documents\NetBeansProjects\ImdbSearch\bin\ImdbSearch.exe %HOMEDRIVE%%HOMEPATH%\Videos\W
+timeout 1
+start %HOMEDRIVE%%HOMEPATH%\Documents\NetBeansProjects\ImdbSearch\bin\ImdbSearch.exe %HOMEDRIVE%%HOMEPATH%\Videos
+
