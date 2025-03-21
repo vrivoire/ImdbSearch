625e4e08408b283a2732f2955e441266ab3c3233
vrivoire <rivoire.vincent@gmail.com>
2025-03-21 07:29:22 -0400

    New PC

--- src/main/resources/Videos.bat
+++ src/main/resources/Videos.bat
@@ -1,6 +1,6 @@
 @echo off
 
-start %HOMEDRIVE%%HOMEPATH%\Documents\NetBeansProjects\ImdbSearch\bin\ImdbSearch.exe E:\Films\W
+start %HOMEDRIVE%%HOMEPATH%\Documents\NetBeansProjects\ImdbSearch\bin\ImdbSearch.exe %HOMEDRIVE%%HOMEPATH%\Videos\W
 timeout 1
-start %HOMEDRIVE%%HOMEPATH%\Documents\NetBeansProjects\ImdbSearch\bin\ImdbSearch.exe E:\Films
+start %HOMEDRIVE%%HOMEPATH%\Documents\NetBeansProjects\ImdbSearch\bin\ImdbSearch.exe %HOMEDRIVE%%HOMEPATH%\Videos
 
